503b91c2fa91fa7b10d26ec38fb0cdde1465a515
RAHUL KUMAR <rajr4712@gmail.com>
2026-05-13 19:50:06 +0530

    Update project data and images in Data.jsx

--- src/component/projects/Data.jsx
+++ src/component/projects/Data.jsx
@@ -1,10 +1,8 @@
-import Image1 from "../../assets/Weather-App-project.gif";
 import Image2 from "../../assets/Ekart-Ecommerse-Website.gif";
 import Image3 from "../../assets/discord-clone.gif";
 import Image4 from "../../assets/Random-Gif-generator.gif";
 import Image5 from "../../assets/Top Course-Project.gif";
 import Image6 from "../../assets/Github profile search project.gif";
-import Image7 from "../../assets/ModernChair-Project.gif";
 import Image8 from "../../assets/MakeMy-tripJourney-project.gif";
 import Image9 from "../../assets/github.gif";
 
@@ -12,30 +10,29 @@ import Image9 from "../../assets/github.gif";
 
 
 export const Data = [
-    {
+     {
       id: 1,
-      image: Image1,
-      title: "Weather APP",
-      description:
-        "Developed a Weather application project that displays current weather information and allows users to search for weather data of any city .Integrated  the OpenWeather API to fetch and display real-time weather data Implemented user-friendly features such as temperature, wind, speed, humidity, clouds, weather etc",
-        link:"https://rajr4712.github.io/Weather_project/",
-    },
-    {
-      id: 2,
-      image: Image2,
-      title: "Ekart Ecommerce website",
-      description:
-        "Developed an Ekart E-Commerce website project using ReactJs, Redux and tailwindCss to create a responsive and user-friendly  beautiful interface. Add to cart, Remove to Cart, Checkout functionality Add.User can navigate between cart and home pages",
-        link:"https://rajr4712.github.io/ShoppingCart_project/",
-    },
-    {
-      id: 3,
       image: Image3,
       title: "Discord website clone",
       description:
         "Developed a Discord clone landing page using HTML, CSS, and Tailwind CSS. I used Tailwind CSS to create a responsive and visually appealing design that closely resembles the original Discord landing page. Create a Navbar section, Home, hero, footer, etc.",
-
         link:"https://rahul-discord-clone-project.netlify.app"
+    },
+     {
+      id: 2,
+      image: Image2,
+      title: "World Atlas(React 19)",
+      description:
+        "Built a responsive React 19 country explorer application with smooth UI updates using useTransition, featuring dynamic country data, real-time API integration with Axios, advanced search/filter/sort functionality, and expandable detail views optimized for a seamless user experience across devices"
+        link:"https://world-atlas-reactapp.vercel.app/"
+    },
+    {
+      id: 3,
+      image: Image5,
+      title: "Top Courses Project",
+      description:
+        "Top Courses Project contain a Courses for different Category. Total 5 category created that is DEvlopment , Bussiness, Design , Lifestyle All. Clicked on Any category u will show Top course related to this category. like icon and Toast applied here . click like icon❤️ a toast display:liked Successfully.",
+        link:"http://localhost:3001/TopCourse_project"
     },
     {
       id: 4,
@@ -45,16 +42,9 @@ export const Data = [
         "Design and Devloped a random gif Generator project . Implemented two sections for generating GIFs, including a Random generator and a tag-based generator which generates gifs by the user input. Create UseGif custom hooks to Avoid the same line of code in both Generate and tag sections both which fetch and display a new Gif every time from the server.",
         link:" https://rajr4712.github.io/randomGif_project/"
     },
+
     {
       id: 5,
-      image: Image5,
-      title: "Top Courses Project",
-      description:
-        "Top Courses Project contain a Courses for different Category. Total 5 category created that is DEvlopment , Bussiness, Design , Lifestyle All. Clicked on Any category u will show Top course related to this category. like icon and Toast applied here . click like icon❤️ a toast display:liked Successfully.",
-        link:"http://localhost:3001/TopCourse_project"
-    },
-    {
-      id: 6,
       image: Image6,
       title: "Github profile search project",
       description:
@@ -62,15 +52,7 @@ export const Data = [
         link:"https://rajr4712.github.io/DevDetective_Project/"
     },
     {
-      id: 7,
-      image: Image7,
-      title: "Modern Chair project",
-      description:
-       "Designed and developed a Modern Chair project using HTML, CSS, and JavaScript.features discounted chair prices, along with Description and Details buttons. Description provides a short overview, Details reveals chair dimensions and weight.6 different color box added by selecting a color instantly updates the chair's appearance on the UI",
-        link: "https://rajr4712.github.io/Modern_chair_project/"
-    },
-    {
-      id: 8,
+      id: 6,
       image: Image8,
       title: "MakeMy TRip Journey Project",
       description:
@@ -78,9 +60,8 @@ export const Data = [
         link: " https://rajr4712.github.io/planwith_love_project/"
     },
     {
-        id: 9,
+        id: 7,
         image: Image9,
-      
       description: <a href="https://github.com/harshaa11th" target="_blank" rel="noreferrer" className="contact_button">Click here for more<i className="bx bx-right-arrow-alt contact_button-icon"></i>
       </a>,
       link:"https://github.com/harshaa11th"
