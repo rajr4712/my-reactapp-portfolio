ac8c01736d75abb4d00fa1e681707dda5fa4a3a0
RAHUL KUMAR <rajr4712@gmail.com>
2026-05-13 13:39:13 +0530

    Update ScrollUp.jsx

--- src/component/scrollUp/ScrollUp.jsx
+++ src/component/scrollUp/ScrollUp.jsx
@@ -5,7 +5,7 @@ const ScrollUp = () => {
 
     return (
         //go to Top of page when click on that icon (dts y # link in anchor tab me 
-         <a href='#' className="scrollUp">  
+         <a href='#home' className="scrollUp">  
             <i className="uil uil-arrow-up scrollUp_icon"></i>
         </a>
     )
